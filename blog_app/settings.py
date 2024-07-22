fc2589f8408f4898064db284c89b673cfba08c5b
LionRouge1 <matchoudiavlessi@gmail.com>
2024-07-22 23:31:29 +0000

    migrate application

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_app_db',
+        'USER': 'crowdfrica',
     }
 }
 
