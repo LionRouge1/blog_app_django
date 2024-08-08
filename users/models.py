850784f19a32c1661a6c1dc839cd6464e6453543
LionRouge1 <matchoudiavlessi@gmail.com>
2024-08-08 12:03:29 +0000

    add additional column to the User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+  photo = models.ImageField(upload_to='profile')
+  bio = models.TextField()
+  posts_counter = models.PositiveIntegerField('posts counter', default=0)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class HttpRequestTest(TestCase):
+  def test_user_index_page_status(self):
+    response = self.client.get(reverse('user:index'))
