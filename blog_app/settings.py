c0281189ef5f9a89927ddebc15ac1d5accb7a738
LionRouge1 <matchoudiavlessi@gmail.com>
2024-08-09 16:15:43 +0000

    add media url

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
-  BASE_DIR / 'static'
+  BASE_DIR / 'static',
+  BASE_DIR / 'media',
 ]
 
 # Default primary key field type
--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-  photo = models.ImageField(upload_to='profiles')
-  bio = models.TextField()
-  posts_counter = models.PositiveIntegerField(default=0)
+  photo = models.ImageField(upload_to='profiles', blank=True, null=True, verbose_name='photo')
+  bio = models.TextField(blank=True, null=True, verbose_name='Biograthy')
+  posts_counter = models.PositiveIntegerField(default=0, null=False, verbose_name='posts counter')
