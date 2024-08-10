913fe0b8f5009b368f30774ec48d8bf078e5e146
LionRouge1 <matchoudiavlessi@gmail.com>
2024-08-10 15:20:52 +0000

    add registration view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
+from django.contrib import   messages
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from .forms import CustomUserCreationForm
 # Create your views here.
 def index_view(request):
-  return render()
+  return render(request, 'users/index.html')
+
+def register(request):
+  # print(dir(request))
+  if request.method == 'POST':
+    form = CustomUserCreationForm()
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Account Created successfully')
+      return HttpResponse()
+    else:
+      messages.error(request, 'Error in creating user')
+  else:
+    # messages.error(request, 'Error in creating user')
+    # print(messages)
+    form = CustomUserCreationForm()
+    context = {
+      'form':form
+    }
+  
+  return render(request, 'users/register.html', context)
