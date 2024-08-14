558be6943a9d263d1b5715b7059c48f8d0ba9115
LionRouge1 <matchoudiavlessi@gmail.com>
2024-08-14 11:34:16 +0000

    Create EditView to edit the profile

--- users/views.py
+++ users/views.py
@@ -2,9 +2,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 # from .forms import CustomUserCreationForm
 # Create your views here.
@@ -18,23 +21,43 @@ class indexView(generic.ListView):
 
   # return render(request, 'users/index.html')
 
-def register(request):
+class SignUpView(generic.CreateView):
+  form_class = CustomUserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'registration/signup.html'
   # print(dir(request))
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    print(form.is_valid())  
-    # if form.is_valid():
-    #   user = form.save()
-    #   messages.success(request, 'Account Created successfully')
-    #   return HttpResponseRedirect(reverse("users:show", args=(user.id)))
-  else:
-    form = UserCreationForm()
+  # if request.method == 'POST':
+  #   form = UserCreationForm(request.POST)
+  #   print(form.is_valid())  
+  #   # if form.is_valid():
+  #   #   user = form.save()
+  #   #   messages.success(request, 'Account Created successfully')
+  #   #   return HttpResponseRedirect(reverse("users:show", args=(user.id)))
+  # else:
+  #   form = UserCreationForm()
 
-  context = {
-    'form':form
-  }
-  return render(request, 'users/register.html', context)
+  # context = {
+  #   'form':form
+  # }
+  # return render(request, 'users/register.html', context)
 
+# def login(request):
+#   if request.method == 'POST':
+#     pass
+#   user = LoginForm()
+#   return render(request, 'users/login.html', {'form':user})
+# @login_required
 def show(request, user_id):
   user = get_object_or_404(User, pk=user_id)
   return render(request, 'users/show.html', { 'user':user})
+
+class EditView(LoginRequiredMixin, generic.edit.UpdateView):
+  model = User
+  form_class = CustomUserChangeForm
+  template_name = 'registration/edit.html'
+
+  def get_object(self):
+    return self.request.user
+  
+  def get_success_url(self):
+    return reverse_lazy('users:show', args=[self.request.user.id])
